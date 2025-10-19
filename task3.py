2e0014622d276429669f4e156c8b053b0808dd72
NasraDecoded <nashomar03@gmail.com>
2025-10-19 22:33:50 +0200

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,24 @@
+#2 numbers saved as variables  
+a = 25
+b= 5
+#addition
+print(a+b)
+
+#subtraction
+print(a-b)
+
+#multiplication
+print(a*b)
+
+#division
+print(a/b)
+
+#modulus
+print(a%b)
+
+#exponentiation
+print(a**b)
+
+
+#floor division
+print(a//b)
