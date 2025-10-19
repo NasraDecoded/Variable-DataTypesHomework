66c337663eb0ebaa5632af0eb9e92744539849ff
NasraDecoded <nashomar03@gmail.com>
2025-10-19 22:33:34 +0200

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,14 @@
+#Save the first and last name as variables
+from os import name
+
+
+first_name="Nasra"
+last_name="Omar"
+
+#contatenate the first and last name name with a space in between
+#full_name=first_name + " " + last_name 
+
+full_name=first_name + " " + last_name
+
+print("Hello ", full_name, "!Welcome to world of decoders.")
+
